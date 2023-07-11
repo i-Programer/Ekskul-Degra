e451e2c8acd50d4b2b382b217e2b8b7f32395e1b
i-Programer <ashof.z17@gmail.com>
2023-07-11 20:06:27 +0700

    fix something in ProjectCard component

--- resources/js/Components/MainComponent/Project/ProjectCard.jsx
+++ resources/js/Components/MainComponent/Project/ProjectCard.jsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 const ProjectCard = ({ file, authorName, onClick }) => {
-    // Extract the first name
     let authorFile = authorName.split(",")[0];
 
-    // If the <br/> tag is not present, extract the name without modification
     if (authorFile === authorName) {
         authorFile = authorName;
     } else {
-        // Trim any whitespace
         authorFile = authorFile.trim();
     }
 
-    // Output the first name
-    console.log(authorFile);
     return (
         <>
             <div
